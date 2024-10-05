eb24a5104bd4152902f1ac608c243dbcbbdd8a33
lbrbence169 <leber.bence08@gmail.com>
2024-10-05 22:12:34 +0200

    KOMMENTWK

--- HOSSZ.py
+++ HOSSZ.py
@@ -23,23 +23,26 @@ elsoleirasa = Label( root , text = "Válassza ki az első mértékegységet ", b
 masodikleirasa = Label( root , text = "Válassza ki a második mértékegységet ",bg="blue",fg="white", borderwidth=5, font=('Comic Sans', 10, 'bold') ) 
 
 
-
+#bekeres
 ertek = Entry(root, width=30, borderwidth=4)
 ertek.grid(row=2, column=1, padx=10, pady=10)
 
+#Mertekegysegek felvétele + listában átváltóérték tárolása
 mertekegysegek = ["Milliméter", "Centiméter", "Deciméter", "Méter", "Kilóméter"]
 mertekegyseg_valtoszam = [["Milliméter", 1], ["Centiméter", 10], ["Deciméte", 100], ["Méter", 1000], ["Kilóméter", 1000000]]
 
+#Hozzárendelni az első legördűlő menün kiválasztott mértékegységet az értékhez
 clicked = StringVar()
 clicked.set(mertekegysegek[0])
 
 legordulo1 = OptionMenu(root, clicked, *mertekegysegek)
 
+#Hozzárendelni az második legördűlő menün kiválasztott mértékegységet az átváltáshoz
 clicked2 = StringVar()
 clicked2.set(mertekegysegek[0])
 legordulo2 = OptionMenu(root, clicked2, *mertekegysegek)
 
-
+#Bekeres
 def bekeres():
     bekert_ertek = ertek.get()
     bekert_ertek = int(bekert_ertek)
@@ -60,12 +63,14 @@ def bekeres():
     veg_info = Label(root, text=f"Az átváltott érték: {bekert_ertek} {mertekegysegek[index]} = {(bekert_ertek * valtoszam)} {mertekegysegek[index2]}",font=('Comic Sans', 14, 'bold')).grid(row=6, column=0, columnspan=2, pady=20)
 
 
+#Meghívások, Gomb az átváltáshoz, leírások a felhasználó könnyebb tájékozódásához 
 myButton = Button(root, text="Bevitel", command=bekeres, padx=40, pady=10, fg="white", bg="blue")
 elsoleirasa.grid(row=3, column=0, padx=10, pady=10)
 legordulo1.grid(row=3, column=1, padx=10, pady=10)
 masodikleirasa.grid(row=4, column=0, padx=10, pady=10)
 legordulo2.grid(row=4, column=1, padx=10, pady=10)
 
+#Végeredmény kiiratása!
 myButton.grid(row=5, column=0, columnspan=2, pady=20)
 
 root.mainloop()
